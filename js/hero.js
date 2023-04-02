15c587a64071f4f4036059e739e52efe06184618
Gamma Microwave <pranavt150@gmail.com>
2023-04-02 16:07:04 +0530

    nav bar style update

--- js/hero.js
+++ js/hero.js
@@ -1,4 +1,5 @@
 window.addEventListener("load", () => {
+  const navContWrapper = document.querySelector(".nav-cont-wrapper");
   const heroContainer = document.querySelector(".hero-container");
   const heroLogo = document.querySelector(".hero-logo");
   const fadeIn = document.querySelectorAll(".fade-in");
@@ -6,6 +7,7 @@ window.addEventListener("load", () => {
     text.style.opacity = 1;
     text.style.transform = "translateY(0)";
   });
+  navContWrapper.style.opacity = 1;
   heroLogo.style.opacity = 1;
   heroContainer.style.opacity = 1;
   heroContainer.style.boxShadow = "inset 0 0 0 1000px rgba(0,0,0,0.5)";
