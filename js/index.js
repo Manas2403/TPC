72dba38662e39bbe2dcfbb0bf76f1f3cb6e2a4b9
Gamma Microwave <pranavt150@gmail.com>
2023-04-11 21:56:04 +0530

    placed navbar on all pages

--- js/index.js
+++ js/index.js
@@ -1,22 +1,3 @@
-const debounce = (fn) => {
-  let frame;
-  return (...params) => {
-    if (frame) {
-      cancelAnimationFrame(frame);
-    }
-    frame = requestAnimationFrame(() => {
-      fn(...params);
-    });
-  };
-};
-
-const storeScroll = () => {
-  document.documentElement.dataset.scroll = window.scrollY;
-};
-
-document.addEventListener("scroll", debounce(storeScroll), { passive: true });
-storeScroll();
-
 document.querySelectorAll('.nav-collapse').forEach((navCollapse) => {
   navCollapse.addEventListener('mouseenter', () => {
     document.querySelectorAll('.nav-collapse').forEach((nc) => {
