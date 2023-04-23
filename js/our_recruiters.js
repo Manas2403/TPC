e1f2c8a3dde6c2a9bd4b6ec75b2b88112da37c04
Gamma Microwave <pranavt150@gmail.com>
2023-04-23 14:12:53 +0530

    made our recruiters page use json

---
+++ js/our_recruiters.js
@@ -0,0 +1,17 @@
+fetch("../json/recruiters.json")
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    const recruitersGrid = document.querySelector("#recruiters_grid");
+    data.data.forEach((recruiter) => {
+      recruitersGrid.innerHTML += `
+        <a class="post" target="_blank" href="${recruiter.companyLink}">
+          <img
+            src=${recruiter.image}
+            alt=${recruiter.companyName}
+          />
+        </a>
+        `;
+    });
+  });
