5fea2f11466f37eb8af04422610b51f683beaa2b
BuddyLongLegs <anurag10jain@gmail.com>
2023-05-01 01:48:45 +0530

    Fixed reach us and changed recruitors page

--- js/our_recruiters.js
+++ js/our_recruiters.js
@@ -6,12 +6,7 @@ fetch("./json/recruiters.json")
     const recruitersGrid = document.querySelector("#recruiters_grid");
     data.data.forEach((recruiter) => {
       recruitersGrid.innerHTML += `
-        <a class="post" target="_blank" href="${recruiter.companyLink}">
-          <img
-            src=${recruiter.image}
-            alt=${recruiter.companyName}
-          />
-        </a>
+        <a href="${recruiter.link}" class="company">${recruiter.name}</a>
         `;
     });
   });
