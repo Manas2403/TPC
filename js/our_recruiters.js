49fec00f1f752c2bb0e00d562996de902b56615b
guptamanas149@gmail.com <guptamanas149@gmail.com>
2023-05-05 02:06:18 +0530

    updated our recruiters page

--- js/our_recruiters.js
+++ js/our_recruiters.js
@@ -1,12 +1,12 @@
 fetch("./json/recruiters.json")
-  .then((response) => {
-    return response.json();
-  })
-  .then((data) => {
-    const recruitersGrid = document.querySelector("#recruiters_grid");
-    data.data.forEach((recruiter) => {
-      recruitersGrid.innerHTML += `
-        <a href="${recruiter.link}" class="company">${recruiter.name}</a>
+    .then((response) => {
+        return response.json();
+    })
+    .then((data) => {
+        const recruitersGrid = document.querySelector("#recruiters_grid");
+        data.data.forEach((recruiter) => {
+            recruitersGrid.innerHTML += `
+        <a href="${recruiter.link}" target="__blank" class="company">${recruiter.name}</a>
         `;
+        });
     });
-  });
